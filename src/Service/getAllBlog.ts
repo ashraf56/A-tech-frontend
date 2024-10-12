611d3f1ca4419af5548fe7098ed5d5c41857f7ee
ashraf56 <ashrafulfahim07@gmail.com>
2024-10-12 15:55:47 +0600

    updated code

--- src/Service/getAllBlog.ts
+++ src/Service/getAllBlog.ts
@@ -2,7 +2,7 @@
 export const getAllBlog = async ()=>{
   const serverAPi = process.env.NEXT_PUBLIC_serverApi
 try {
-    const res = await fetch(`${serverAPi}/blogs`,{ next:{revalidate:1000}} );
+    const res = await fetch(`${serverAPi}/blogs`,{ cache:"no-store"} );
       
     if (!res.ok) {
       throw new Error("data load failed");
